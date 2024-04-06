3311ed2bc0b77fceff52129a2d167ff58b5678fc
afpsargeant456 <166194194+afpsargeant456@users.noreply.github.com>
2024-04-06 14:30:11 +1100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching over Designs Central"];
+const statusMessages = ["Watching over **Designs Central**"];
 
 
 let currentIndex = 0;
