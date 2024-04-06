3a85e8594cab84b8867973435ab52622b1c65f69
afpsargeant456 <166194194+afpsargeant456@users.noreply.github.com>
2024-04-06 14:22:37 +1100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching over Designs Central"];
 
 
 let currentIndex = 0;
