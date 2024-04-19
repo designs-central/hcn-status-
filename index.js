d1abc0103cc70c945cb5bfe1c9e4c5a11376c469
afpsargeant456 <166194194+afpsargeant456@users.noreply.github.com>
2024-04-19 15:37:46 +1000

    dc status updater

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["WATCHING","DESIGNS CENTRAL"];
+const statusMessages = ["Watching Designs Central","discord.gg/designscentral"];
 
 
 let currentIndex = 0;
